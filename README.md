2f5bc17f25469b329d47635c05774694b28729fe
Yuri Costa <yuricosta2319@gmail.com>
2024-07-22 20:16:08 -0300

    início - Calculadora Simples

---
+++ README.md
@@ -0,0 +1,46 @@
+# Calculadora Simples
+
+Este projeto é uma calculadora simples desenvolvida como parte de um desafio de programação no TIC em Trilhas, na trilha de Pensamento Computacional para o programa Geração Caldeira.
+
+## Linguagem Usada:
+<img src="https://cdn-icons-png.flaticon.com/512/1822/1822899.png" alt="Python Logo" width="50">
+
+## Funcionalidades
+
+A calculadora realiza as seguintes operações:
+
+- Soma
+- Subtração
+- Multiplicação
+- Divisão
+
+### Exemplo de Uso
+
+1. Execute o script:
+    ```bash
+    python calculadora.py
+    ```
+
+2. Você verá um menu com as opções disponíveis:
+    ```
+    Calculadora Simples
+    
+    1 - Soma
+    2 - Subtração
+    3 - Multiplicação
+    4 - Divisão
+    s - Sair
+    ```
+
+3. Selecione a operação desejada digitando o número correspondente ou "s" para sair.
+
+4. Insira os números conforme solicitado e veja o resultado da operação.
+
+## Observações
+
+- Para operações de divisão, não é permitido dividir por zero.
+- Este projeto foi desenvolvido para fins educativos e como prática de programação iniciante.
+
+## Créditos
+
+Este projeto foi desenvolvido como parte do desafio de programação no TIC em Trilhas, trilha de Pensamento Computacional para o programa Geração Caldeira.
---
+++ calculadora.py
@@ -0,0 +1,51 @@
+import os
+
+def limpar_tela():
+    os.system('cls')
+
+def calculadora():
+    while True:
+        print('\nCalculadora Simples\n')
+        print('1 - Soma')
+        print('2 - Subtração')
+        print('3 - Multiplicação')
+        print('4 - Divisão')
+        print('s - Sair')
+        operacao = input('\nSelecione uma opção ou "s" para sair: \n')
+
+        if operacao == 's' or operacao == 'S':
+            limpar_tela()
+            print('\nObrigado por utilizar a Calculadora Simples!')
+            break
+
+        if operacao not in ['1','2','3','4']:
+            limpar_tela()
+            print('\nOpção inválida! Tente novamente!')
+            continue
+
+        numero_1 = float(input('\nDigite o primeiro número: '))
+        numero_2 = float(input('\nDigite o segundo número: '))
+
+        if operacao == '1':
+            resultado = numero_1 + numero_2
+            limpar_tela()
+            print(f'\nO resultado da operação soma é: {resultado}')
+        elif operacao == '2':
+            resultado = numero_1 - numero_2
+            limpar_tela()
+            print(f'\nO resultado da operação subtração é: {resultado}')
+        elif operacao == '3':
+            resultado = numero_1 * numero_2
+            limpar_tela()
+            print(f'\nO resultado da operação multiplicação é: {resultado}')
+        else:
+            if numero_2 == 0:
+                print('\nDivisões por zero não são possiveis. Tente novamente')
+                continue
+            else:
+                resultado = numero_1 / numero_2
+                limpar_tela()
+                print(f'\nO resultado da operação divisão é: {resultado}')
+
+if __name__ == '__main__':
+    calculadora()
